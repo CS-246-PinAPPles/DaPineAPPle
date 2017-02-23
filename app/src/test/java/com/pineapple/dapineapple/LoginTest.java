c69b28e0202788afe6139c60b44355ca22769408
Dillon <dillongc21@gmail.com>
2017-02-22 20:43:36 -0700

    added 2 more unit tests

--- app/src/test/java/com/pineapple/dapineapple/LoginTest.java
+++ app/src/test/java/com/pineapple/dapineapple/LoginTest.java
@@ -2,12 +2,8 @@ package com.pineapple.dapineapple;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
- * Example local unit test, which will execute on the development machine (host).
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class LoginTest {
     @Test

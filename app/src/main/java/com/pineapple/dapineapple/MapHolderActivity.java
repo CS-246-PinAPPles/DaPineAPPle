dea885db0c7716c4906110a65717844bf4ee05a8
Brayan Vilorio <vil13003@byui.edu>
2017-03-04 20:19:12 -0700

    //map and menu commit

---
+++ app/src/main/java/com/pineapple/dapineapple/MapHolderActivity.java
@@ -0,0 +1,13 @@
+package com.pineapple.dapineapple;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapHolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_holder);
+    }
+}

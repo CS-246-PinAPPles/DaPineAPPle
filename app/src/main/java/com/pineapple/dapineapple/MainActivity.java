2b5b6f5e553636274a2d3f230c984160c86013b6
Brayan Vilorio <vil13003@byui.edu>
2017-03-06 09:29:49 -0700

    Working on map holder

--- app/src/main/java/com/pineapple/dapineapple/MainActivity.java
+++ app/src/main/java/com/pineapple/dapineapple/MainActivity.java
@@ -1,7 +1,7 @@
 package com.pineapple.dapineapple;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 

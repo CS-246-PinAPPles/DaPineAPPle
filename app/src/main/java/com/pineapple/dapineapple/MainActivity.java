7309f5b80746ab7bc0994f3a5483a26532e28eae
gdaleigna <del15027@byui.edu>
2017-03-08 09:33:25 -0700

    TeamActivity 10
    Added a simple log in our MainActivity.

--- app/src/main/java/com/pineapple/dapineapple/MainActivity.java
+++ app/src/main/java/com/pineapple/dapineapple/MainActivity.java
@@ -2,12 +2,17 @@ package com.pineapple.dapineapple;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        Log.println(1, TAG, ": created successfully.");
         setContentView(R.layout.activity_main);
     }
 }

07b1b60f864f2a3aaeff27ae3bbe47ad26dfc3da
dillonchristensen <Dillongc21@gmail.com>
2017-03-27 09:56:11 -0600

    added mail icon stuff

--- app/src/main/java/com/pineapple/dapineapple/MainActivity.java
+++ app/src/main/java/com/pineapple/dapineapple/MainActivity.java
@@ -33,11 +33,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //remove later/****************************************/
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        /******************************************************/
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
